1f2374414f39e2695ecc95c815429bf3fd6467fd
kirk <vonkratzmann@gmail.com>
2018-06-05 18:35:02 +1000

    Updated version with shared preferences

--- app/src/main/java/kk/speeddisplay/MainActivity.java
+++ app/src/main/java/kk/speeddisplay/MainActivity.java
@@ -34,7 +34,7 @@ import java.util.Locale;
  * MainActivity
  * <p>
  */
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements SharedPreferences.OnSharedPreferenceChangeListener {
 
     /* get a tag for output debugging */
     private final static String TAG = MainActivity.class.getSimpleName();
@@ -49,14 +49,12 @@ public class MainActivity extends AppCompatActivity {
      * these are saved in the shared preferences
      */
     private float maxSpeed;
-    private long activityActiveUpdateRate;
-    private long activityNotActiveUpdateRate;
+    private long activityRunningUpdateRate;
+    private final static String RunningUpdateRateDefault = "2";
 
-    /* default values in milliseconds for the update intervals at which the activity will receive location updates */
-    private final static long ACTIVITY_ACTIVE_UPDATE_RATE_DEFAULT = 1000L;
-    private final static long ACTIVITY_NOT_ACTIVE_UPDATE_RATE_DEFAULT = 30000L;
+    private long activityNotRunningUpdateRate;
+    private final static String NotRunningUpdateRateDefault = "30";
 
-    private SharedPreferences mSharedPref;
     private com.google.android.gms.location.FusedLocationProviderClient mFusedLocationClient;
     private LocationRequest mLocationRequest;
     private LocationCallback mLocationCallback;
@@ -87,8 +85,9 @@ public class MainActivity extends AppCompatActivity {
                 }
                 /* process all locations provided */
                 for (Location location : locationResult.getLocations()) {
-                    displaySpeed(location.getSpeed());
-                    checkMaxSpeed(location.getSpeed());
+                    float speed = location.getSpeed();
+                    displaySpeed(speed);
+                    checkMaxSpeed(speed);
                 }
             }
         };
@@ -118,23 +117,28 @@ public class MainActivity extends AppCompatActivity {
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
-        if (id == R.id.max_reset) {                                                 //reset maximum speed?
-            SharedPreferences.Editor mEditor = mSharedPref.edit();                    //yes, save to shared preferences
+        /* check if request to reset maximum speed */
+        if (id == R.id.max_reset) {
+            /* save the new speed in shared preferences */
+            SharedPreferences mSharedPref = PreferenceManager.getDefaultSharedPreferences(this);
+            SharedPreferences.Editor mEditor = mSharedPref.edit();
             mEditor.clear();
-            mEditor.putFloat(getString(R.string.pref_saved_max_speed_key), 0f);                                   //zero saved maximum speed
+            mEditor.putFloat(getString(R.string.pref_saved_max_speed_key), 0f);
             mEditor.apply();
+            /* display the zeroed speed */
             maxSpeed = 0f;
-            tvMaxSpeed.setText(String.format(Locale.UK, "%1$.1f km/hr", 0f)); //zero display of maximum speed
+            tvMaxSpeed.setText(String.format(Locale.UK, "%1$.1f km/hr", 0f));
             return true;
         }
-
+        /* check if request to navigate to the settings scren */
         if (id == R.id.action_settings) {
             Intent startSettingsActivity = new Intent(this, SettingsActivity.class);
             startActivity(startSettingsActivity);
             return true;
         }
-        if (id == R.id.quit) {                                                       //request to exit/quit?
-            finish();                                                                //yes, terminate the program
+        /* check if request to exit */
+        if (id == R.id.quit) {
+            finish();
             return true;
         }
         return super.onOptionsItemSelected(item);
@@ -165,62 +169,135 @@ public class MainActivity extends AppCompatActivity {
     }
 
     /**
-     * Returns an Image object that can then be painted on the screen.
-     * <p>
-     * This method always returns immediately, whether or not the
-     * image exists. When this applet attempts to draw the image on
+     * Retrieves shared preferences for maximum speed & location provider update rate
      */
     private void setupSharedPreferences() {
         SharedPreferences mSharedPref = PreferenceManager.getDefaultSharedPreferences(this);
-        /* Get all of the values from shared preferences to set it up */
+        /* Get the values from shared preferences and store in appropriate variables */
+        updatePrefMaxSpeed();
+        updatePrefRunningRate(mSharedPref, getString(R.string.pref_running_update_rate_key));
+        updatePrefNotRunningRate(mSharedPref, getString(R.string.pref_not_running_update_rate_key));
+        /* register listener here for any changes and unregister in onDestroy */
+        mSharedPref.registerOnSharedPreferenceChangeListener(this);
+    }
 
-
-        //setupSharedPrefMaxSpeed();
-        //setupSharedPrefAppActive();
-        //setupSharedPrefAppNotActive();
+    @Override
+    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
+        if (key.equals(getString(R.string.pref_running_update_rate_key))) {
+            updatePrefRunningRate(sharedPreferences, key);
+        } else if (key.equals(getString(R.string.pref_not_running_update_rate_key))) {
+            updatePrefNotRunningRate(sharedPreferences, key);
+        }
     }
 
     /**
-     * Retrieves saved maximum speed detected to date from Shared preferences
+     * Retrieves saved maximum speed detected t from Shared preferences
      * and then displays this speed
      */
-    private void setupSharedPrefMaxSpeed() {
+    private void updatePrefMaxSpeed() {
+        SharedPreferences mSharedPref = PreferenceManager.getDefaultSharedPreferences(this);
         maxSpeed = mSharedPref.getFloat(getString(R.string.pref_saved_max_speed_key), 0.0F);
-        Log.d(TAG, "SharedPref MaxSpeed: " + Float.toString(maxSpeed));
+        Log.d(TAG, "updatePrefMaxSpeed maxSpeed: " + Float.toString(maxSpeed));
         /* display max speed */
         tvMaxSpeed.setText(String.format(Locale.UK, getString(R.string.units_and_number_of_decimals), maxSpeed));
     }
 
     /**
-     * Retrieves update rate from Shared preferences for when activity is active
+     * Updates rate at which location provider provides updates when the activity is running
+     * gets the new rate fom the share preferences
+     * checks valid format
+     * converts it to milliseconds and saves it
+     *
+     * @param sharedPreferences SharedPreference object where preferences are stored
+     * @param key               preference key
      */
-    private void setupSharedPrefAppActive() {
-        activityActiveUpdateRate = mSharedPref.getLong(getString(R.string.pref_activity_active_update_rate_key), ACTIVITY_ACTIVE_UPDATE_RATE_DEFAULT);
-        Log.d(TAG, "SharedPref App active update rate: " + Float.toString(activityActiveUpdateRate));
-        setLocationUpdateRate(activityActiveUpdateRate);
+    private void updatePrefRunningRate(SharedPreferences sharedPreferences, String key) {
+        /* get the rate and convert to milliseconds and save it */
+        String updateRate = sharedPreferences.getString(key, RunningUpdateRateDefault);
+        Float rate;
+        if (checkFloatFormat(updateRate)) {
+            rate = Float.valueOf(updateRate);
+        } else {
+            rate = Float.valueOf(RunningUpdateRateDefault);
+        }
+        Log.d(TAG, "updatePrefRunningRate rate: " + rate);
+
+        /* convert to milliseconds and store as a long*/
+        rate = rate * 1000f;
+        activityRunningUpdateRate = rate.longValue();
+        Log.d(TAG, "updatePrefRunningRate activityRunningUpdateRate: " + activityRunningUpdateRate);
+        /* If activity is currently running then update location provider */
+        if (isActivityRunning()) {
+            setLocationUpdateRate(activityRunningUpdateRate);
+        }
     }
 
     /**
-     * Retrieves update rate from Shared preferences for when aactivity is not active
+     * Updates rate at which location provider provides updates when the activity is not running
+     * gets the new rate fom the share preferences
+     * checks valid format
+     * converts it to milliseconds and saves it
+     * getting the new rate fom the share preferences and
+     * then updating the rate variable
+     *
+     * @param sharedPreferences SharedPreference object where preferences are stored
+     * @param key               preference key
      */
-    private void setupSharedPrefAppNotActive() {
-        activityNotActiveUpdateRate = mSharedPref.getLong(getString(R.string.pref_activity_not_active_update_rate_key), ACTIVITY_NOT_ACTIVE_UPDATE_RATE_DEFAULT);
-        Log.d(TAG, "SharedPref App not active update rate: " + Float.toString(activityNotActiveUpdateRate));
-        setLocationUpdateRate(activityNotActiveUpdateRate);
+    private void updatePrefNotRunningRate(SharedPreferences sharedPreferences, String key) {
+        /* get the rate and convert to milliseconds and save it*/
+        String updateRate = sharedPreferences.getString(key, NotRunningUpdateRateDefault);
+        Float rate;
+        if (checkFloatFormat(updateRate)) {
+            rate = Float.valueOf(updateRate);
+        } else {
+            rate = Float.valueOf(NotRunningUpdateRateDefault);
+        }
+        Log.d(TAG, "getPrefNotRunningRate rate: " + rate);
+
+        /* convert to milliseconds and store as a long*/
+        rate = rate * 1000f;
+        activityNotRunningUpdateRate = rate.longValue();
+
+        Log.d(TAG, "updatePrefNotRunningRate activityNotRunningUpdateRate: " + activityNotRunningUpdateRate);
+        /* If activity is currently not running then update location provider */
+        if (!isActivityRunning()) {
+            setLocationUpdateRate(activityNotRunningUpdateRate);
+        }
+    }
+
+
+    /**
+     * Check strings represents a valid floating point number
+     * and its value is greater than zero and less than the largest Long number
+     * (why anybody would want such a big number, I don't know)
+     *
+     * @param textNumber String which represents a floating point number
+     * @return true if strings representa valid float number
+     */
+    public static boolean checkFloatFormat(String textNumber) {
+        try {
+            Float number = Float.valueOf(textNumber);
+            if (number <= 0 || number >= Long.MAX_VALUE) {
+                return false;
+            }
+        } catch (NumberFormatException nfe) {
+            return false;
+        }
+        return true;
     }
 
     /**
      * Updates the rate at which the location provider provides updates on location
-     * always sets the accurcay to high
+     * always sets the accuracy to high
      *
-     * @param updateRate rate at which location provider provides updates
+     * @param rate rate at which location provider provides updates
      */
-    private void setLocationUpdateRate(long updateRate) {
+    private void setLocationUpdateRate(long rate) {
+        Log.d(TAG, "setLocationUpdateRate: rate: " + rate);
         mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
-        mLocationRequest.setInterval(updateRate);
-        mLocationRequest.setFastestInterval(updateRate);
+        mLocationRequest.setInterval(rate);
+        mLocationRequest.setFastestInterval(rate);
     }
-
 
     protected void onStart() {
         super.onStart();
@@ -228,21 +305,51 @@ public class MainActivity extends AppCompatActivity {
     }
 
     /**
-     * Sets the location update intervals to the activity active values
+     * Sets the location update intervals to the activity running values
+     * updates a boolean key in shared preferences to say the activity is running
      */
     protected void onResume() {
         super.onResume();
         Log.d(TAG, "onResume");
-        setLocationUpdateRate(activityActiveUpdateRate);
+        setLocationUpdateRate(activityRunningUpdateRate);
+        // Update shared preference to say activity is running
+        SharedPreferences sp = getSharedPreferences(getString(R.string.pref_activity_state_key), MODE_PRIVATE);
+        SharedPreferences.Editor ed = sp.edit();
+        ed.putBoolean(getString(R.string.pref_activity_state_key), true);
+        ed.commit();
     }
 
     /**
-     * Sets the location update intervals to the activity not active values
+     * Sets the location update intervals to the activity not running values
+     * updates a boolean key in shared preferences to say the activity is not running
      */
     protected void onPause() {
         super.onPause();
         Log.d(TAG, "onPause");
-        setLocationUpdateRate(activityNotActiveUpdateRate);
+        /* set the update rate in the location provider to the not running value*/
+        setLocationUpdateRate(activityNotRunningUpdateRate);
+        // Update shared preference to say activity is not running
+        SharedPreferences sp = getSharedPreferences(getString(R.string.pref_activity_state_key), MODE_PRIVATE);
+        SharedPreferences.Editor ed = sp.edit();
+        ed.putBoolean(getString(R.string.pref_activity_state_key), false);
+        ed.commit();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        PreferenceManager.getDefaultSharedPreferences(this).unregisterOnSharedPreferenceChangeListener(this);
+    }
+
+    /**
+     * Checks state of activity ie is it running by accessing a shared preferences key
+     * which is set to true by onResume() and set to false by onPause()
+     *
+     * @return
+     */
+    boolean isActivityRunning() {
+        SharedPreferences sp = getSharedPreferences(getString(R.string.pref_activity_state_key), MODE_PRIVATE);
+        return sp.getBoolean(getString(R.string.pref_activity_state_key), false);
     }
 
     /**
@@ -284,9 +391,11 @@ public class MainActivity extends AppCompatActivity {
         if (speed > maxSpeed) {
             /* we have a new maximum speed */
             maxSpeed = speed;
+            Log.d(TAG, "checkMaxSpeed maxSpeed: " + maxSpeed);
             /* save maximum speed to shared preferences */
+            SharedPreferences mSharedPref = PreferenceManager.getDefaultSharedPreferences(this);
             SharedPreferences.Editor mEditor = mSharedPref.edit();
-            mEditor.clear().putFloat(getResources().getString(R.string.pref_saved_max_speed_key), maxSpeed).apply();
+            mEditor.clear().putFloat(getString(R.string.pref_saved_max_speed_key), maxSpeed).apply();
             /* display new maximum speed */
             tvMaxSpeed.setText(String.format(Locale.UK, getResources().getString(R.string.units_and_number_of_decimals), maxSpeed));
         }
--- app/src/main/java/kk/speeddisplay/SettingsFragment.java
+++ app/src/main/java/kk/speeddisplay/SettingsFragment.java
@@ -1,16 +1,108 @@
 package kk.speeddisplay;
 
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.support.v7.preference.CheckBoxPreference;
+import android.support.v7.preference.EditTextPreference;
+import android.support.v7.preference.ListPreference;
+import android.support.v7.preference.Preference;
 import android.support.v7.preference.PreferenceFragmentCompat;
+import android.support.v7.preference.PreferenceScreen;
+import android.widget.Toast;
+
+import static kk.speeddisplay.MainActivity.checkFloatFormat;
 
 
-public class SettingsFragment extends PreferenceFragmentCompat {
+public class SettingsFragment extends PreferenceFragmentCompat implements
+        SharedPreferences.OnSharedPreferenceChangeListener, Preference.OnPreferenceChangeListener {
 
     @Override
     public void onCreatePreferences(Bundle bundle, String rootKey) {
 
         // Load the preferences from an XML resource
         addPreferencesFromResource(R.xml.pref_speed);
+
+        SharedPreferences sharedPreferences = getPreferenceScreen().getSharedPreferences();
+        PreferenceScreen prefScreen = getPreferenceScreen();
+        int count = prefScreen.getPreferenceCount();
+
+        // Go through all of the preferences, and set up their preference summary.
+        for (int i = 0; i < count; i++) {
+            Preference p = prefScreen.getPreference(i);
+            // You don't need to set up preference summaries for checkbox preferences because
+            // they are already set up in xml using summaryOff and summary On
+            if (!(p instanceof CheckBoxPreference)) {
+                String value = sharedPreferences.getString(p.getKey(), "");
+                setPreferenceSummary(p, value);
+            }
+        }
+        /* Attach the listener to the preferences */
+        Preference preference = findPreference(getString(R.string.pref_running_update_rate_key));
+        preference.setOnPreferenceChangeListener(this);
+
+        preference = findPreference(getString(R.string.pref_not_running_update_rate_key));
+        preference.setOnPreferenceChangeListener(this);
+    }
+
+    /**
+     * Updates the summary for the preference
+     *
+     * @param preference The preference to be updated
+     * @param value      The value that the preference was updated to
+     */
+    private void setPreferenceSummary(Preference preference, String value) {
+        // COMPLETED (3) Don't forget to add code here to properly set the summary for an EditTextPreference
+        if (preference instanceof ListPreference) {
+            // For list preferences, figure out the label of the selected value
+            ListPreference listPreference = (ListPreference) preference;
+            int prefIndex = listPreference.findIndexOfValue(value);
+            if (prefIndex >= 0) {
+                // Set the summary to that label
+                listPreference.setSummary(listPreference.getEntries()[prefIndex]);
+            }
+        } else if (preference instanceof EditTextPreference) {
+            // For EditTextPreferences, set the summary to the value's simple string representation.
+            preference.setSummary(value);
+        }
+    }
+
+    @Override
+    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
+        /* Figure out which preference has changed */
+        Preference preference = findPreference(key);
+
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        Toast error = Toast.makeText(getContext(), "Invalid number format", Toast.LENGTH_SHORT);
+
+        String runningRateKey = getString(R.string.pref_running_update_rate_key);
+        if (preference.getKey().equals(runningRateKey)) {
+            String rate = (String) newValue;
+
+            if (checkFloatFormat(rate)) {
+                return true;
+            } else {
+                error.show();
+                return false;
+            }
+        }
+        return true;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getPreferenceScreen().getSharedPreferences()
+                .registerOnSharedPreferenceChangeListener(this);
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        getPreferenceScreen().getSharedPreferences()
+                .unregisterOnSharedPreferenceChangeListener(this);
     }
 }
